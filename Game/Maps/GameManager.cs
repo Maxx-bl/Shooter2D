8d6aa57f039bbc2b49da353e7a33888be99afaa8
Maxx-bl <max13.bl@gmail.comm>
2024-05-01 13:30:21 +0200

    ctn

--- Game/Maps/GameManager.cs
+++ Game/Maps/GameManager.cs
@@ -4,10 +4,29 @@ using System;
 public class GameManager : Node2D
 {
 
+    Node2D SpawnPoint;
+
     public override void _Ready()
     {
-        PackedScene playerScene = GD.Load<PackedScene>("res://Game/Characters/Player.tscn");
+        InstantiateMap();
+
+        SpawnPoint = GetNode<Node2D>("TileMap/Spawns/P1");
+
+        InstantiatePlayer();
+    }
+
+    void InstantiateMap()
+    {
+        PackedScene mapScene = GD.Load<PackedScene>("res://Game/Maps/SoloMode.tscn");
+        TileMap map = mapScene.Instance<TileMap>();
+        AddChild(map);
+    }
+
+    void InstantiatePlayer()
+    {
+        PackedScene playerScene = GD.Load<PackedScene>("res://Game/Characters/Player/Player.tscn");
         KinematicBody2D player = playerScene.Instance<KinematicBody2D>();
+        player.Position = SpawnPoint.Position;
         AddChild(player);
     }
 
--- Game/Missile/Missile.cs
+++ Game/Missile/Missile.cs
@@ -6,11 +6,22 @@ public class Missile : KinematicBody2D
     public float Speed { get; set; } = 400;
     public Vector2 Target { get; set; }
     public Vector2 Cannon { get; set; }
+    public Vector2 Direction { get; set; }
     
 
     public override void _Ready()
     {
-        Vector2 direction = Target - Cannon;
-        Rotation = direction.Angle();
+        Direction = Target - Cannon;
+        Rotation = Direction.Angle();
+    }
+
+    public override void _PhysicsProcess(float delta)
+    {
+        MoveAndSlide(Direction.Normalized() * Speed);
+    }
+
+    void _on_Area2D_body_entered(Node body)
+    {
+        QueueFree();
     }
 }
